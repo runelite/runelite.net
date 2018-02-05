a0f1b31c4d4a9ea5fd4f6aa24e36697a1ec58cf1
Tomas Slusny <slusnucky@gmail.com>
2018-02-05 14:22:21 +0100

    Disable serviceWorker
    Disable service worker in order to reduce page caching.
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux'
 import createHistory from 'history/createBrowserHistory'
 import App from './components/app'
 import configureStore from './redux/store'
-import registerServiceWorker from './service-worker'
 
 // Create a history of your choosing (we're using a browser history in this case)
 const history = createHistory()
@@ -18,5 +17,3 @@ render(
   </Provider>,
   document.getElementById('root')
 )
-
-registerServiceWorker()
