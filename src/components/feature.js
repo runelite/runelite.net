3b07177fe092574e0aad6f350746763cf9ab4cc4
Tomas Slusny <slusnucky@gmail.com>
2018-02-13 18:25:27 +0100

    Make the feature column numbers change
    Make the feature column numbers change based on device. For example on
    extra small device, only 1 column will be shown in row.
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/components/feature.js
+++ src/components/feature.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Col, Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap'
 
 const Feature = ({ image, title, description }) => (
-  <Col md={4} style={{ marginBottom: 15 }}>
+  <Col xl='3' lg='3' md='4' sm='6' xs='12' style={{ marginBottom: 15 }}>
     <Card>
       <CardImg top src={image} />
       <CardBody>
