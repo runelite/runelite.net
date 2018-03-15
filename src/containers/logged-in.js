bd571241cfb5bb8fc6410fc61cb9dea881fbcfda
Eric Watkins <ewatkins90@gmail.com>
2018-03-14 23:18:34 -0500

    Fix spelling and grammar mistakes

--- src/containers/logged-in.js
+++ src/containers/logged-in.js
@@ -6,13 +6,13 @@ import hero from '../_data/hero'
 const LoggedIn = ({ children }) => (
   <div>
     <Helmet>
-      <title>Succesfully logged in - {hero.title}</title>
+      <title>Successfully logged in - {hero.title}</title>
     </Helmet>
     <Layout>
       <h1>Congratulations!</h1>
       <hr />
       <div>
-        You succesfully logged into the RuneLite. You can now close this window.
+        You have successfully logged into RuneLite. You may now close this window.
       </div>
       {children}
     </Layout>
