3cf7979b4996791bedb63b31abe1b21cb0522f6a
Jeremy Plsek <jeremyplsek@gmail.com>
2018-05-13 11:56:26 -0400

    Add wiki links to all features

--- src/components/feature.js
+++ src/components/feature.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Col, Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap'
 
-const Feature = ({ image, title, description }) => (
+const Feature = ({ image, title, description, link }) => (
   <Col xl='3' lg='3' md='4' sm='6' xs='12' style={{ marginBottom: 15 }}>
     <Card>
       <CardImg top src={image} />
       <CardBody>
-        <CardTitle>{title}</CardTitle>
+        <CardTitle><a href={link}>{title}</a></CardTitle>
         <CardText>{description}</CardText>
       </CardBody>
     </Card>
