938492740b55eacc4da1989d9201e66c3c85477c
Tomas Slusny <slusnucky@gmail.com>
2018-05-26 02:26:30 +0200

    Add support for hours and minutes to blogs
    For correctly displaying the date of the blog posts, add support for
    hours and minutes.
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/blog.js
+++ src/blog.js
@@ -3,18 +3,42 @@ const webpackRequireContext = require.context('!markdown-with-front-matter-loade
 const blog = webpackRequireContext.keys().sort().reverse().reduce((memo, fileName) => {
   // frontmatter and content (actual markdown is loaded on '__content', frontmatter is right on root)
   const frontMatterMarkdown = webpackRequireContext(fileName)
+
   // remove cd and extension
   fileName = fileName.match(/\.\/([\w\d-.]+)\.md/)[1]
-  // extract year and path
-  let tokenizedFilename = fileName.match(/^(\d{4}-\d{2}-\d{2})(.*)/)
+
+  // extract date and path
+  const tokenizedFilename = fileName.match(/^(\d{4}-\d{2}-\d{2})-(\d{2}-\d{2})(.*)/)
+
   // validation
-  if (!tokenizedFilename && !tokenizedFilename[1]) throw new Error('no ^YYYY-MM-DD date in blog filename')
+  if (!tokenizedFilename ||
+    !tokenizedFilename[1] ||
+    !tokenizedFilename[2] ||
+    !tokenizedFilename[3]) {
+    throw new Error('no ^YYYY-MM-DD-HH-mm date in blog filename')
+  }
 
-  let date = tokenizedFilename[1]
-  let name = tokenizedFilename[2]
-  let path = date + name
+  const date = tokenizedFilename[1]
+  const time = tokenizedFilename[2]
+  const name = tokenizedFilename[3]
+  const pathString = date + name
+  const dateTime = date + '-' + time
+  const dateArray = dateTime.split('-')
 
-  return memo.set(path, Object.assign({date: date}, frontMatterMarkdown))
+  // parse date
+  const dateObject = new Date(Date.UTC(
+    // Year
+    parseInt(dateArray[0]),
+    // Month
+    parseInt(dateArray[1]) - 1,
+    // Day
+    parseInt(dateArray[2]),
+    // Hour
+    parseInt(dateArray[3]),
+    // Minute
+    parseInt(dateArray[4])))
+
+  return memo.set(pathString, Object.assign({date: dateObject}, frontMatterMarkdown))
 }, new Map())
 
 export const getLatest = () => blog.values().next().value
