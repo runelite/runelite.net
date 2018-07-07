33a7dcee8902507388624109e1b37423642dd366
Adam <Adam@anope.org>
2018-07-07 12:57:40 -0400

    Update launcher to 1.6.0

--- src/_data/hero.js
+++ src/_data/hero.js
@@ -17,7 +17,7 @@ export default {
   ],
   buttons: [
     {
-      link: 'https://github.com/runelite/launcher/releases/download/1.5.2/RuneLiteSetup32.exe',
+      link: 'https://github.com/runelite/launcher/releases/download/1.6.0/RuneLiteSetup32.exe',
       icon: faWindows,
       text: 'Download for Windows (32 bit)',
       color: 'primary',
@@ -25,7 +25,7 @@ export default {
       os: 'Windows32'
     },
     {
-      link: 'https://github.com/runelite/launcher/releases/download/1.5.2/RuneLiteSetup.exe',
+      link: 'https://github.com/runelite/launcher/releases/download/1.6.0/RuneLiteSetup.exe',
       icon: faWindows,
       text: 'Download for Windows (64 bit)',
       color: 'primary',
@@ -33,7 +33,7 @@ export default {
       os: 'Windows64'
     },
     {
-      link: 'https://github.com/runelite/launcher/releases/download/1.5.2/RuneLite.dmg',
+      link: 'https://github.com/runelite/launcher/releases/download/1.6.0/RuneLite.dmg',
       icon: faApple,
       text: 'Download for macOS',
       color: 'secondary',
@@ -41,7 +41,7 @@ export default {
       os: 'macOS'
     },
     {
-      link: 'https://github.com/runelite/launcher/releases/download/1.5.2/RuneLite.jar',
+      link: 'https://github.com/runelite/launcher/releases/download/1.6.0/RuneLite.jar',
       icon: faCoffee,
       text: 'Download for all platforms',
       color: 'success',
