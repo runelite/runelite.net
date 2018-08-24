42f6dfb3fea8fd9fae27be0fe7a673de8d592028
Tomas Slusny <slusnucky@gmail.com>
2018-08-24 20:06:08 +0200

    Replace react-snap with smaller webpack prerender
    Replace big and slow react-snap pre-renderer with smaller webpack
    prerender plugin.
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,19 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
 const rewirePreact = require('react-app-rewire-preact')
 
 /* config-overrides.js */
 module.exports = function override (config, env) {
   config = rewirePreact(config, env)
+
+  config.plugins.push(
+    new PrerenderSPAPlugin({
+      // Required - The path to the webpack-outputted app to prerender.
+      staticDir: path.join(__dirname, 'build'),
+      // Required - Routes to render.
+      routes: ['/', '/features', '/blog']
+    })
+  )
+
   return config
 }
