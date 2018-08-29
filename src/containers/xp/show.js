d6344dd9bb04d00ea5157824ed9cc6131d781497
Tomas Slusny <slusnucky@gmail.com>
2018-08-29 11:38:00 +0200

    Remove react-chartjs2, use just the lib
    - Remove react-chartjs-2, use simply just chart.js
    - Fix updating of charts in XP tracker (doable now without
    react-chartjs-2)
    - Fix global config for chart.js from outdated version
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/containers/xp/show.js
+++ src/containers/xp/show.js
@@ -1,27 +1,17 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { Helmet } from 'react-helmet'
+import {connect} from 'react-redux'
+import {Helmet} from 'react-helmet'
 import Chart from 'chart.js'
-import {Bar, Line} from 'react-chartjs-2'
 import Layout from '../../components/layout'
 import hero from '../../_data/hero'
 import {
   allRanksSelector, allXpSelector, nameSelector, skillRankSelector,
   skillXpSelector, ranksSelector, skillSelector
 } from '../../redux/modules/runelite'
+import {NavLink} from 'redux-first-router-link'
 
-Chart.defaults.options = {
-  responsive: true,
-  maintainAspectRatio: false,
-  animation: {
-    duration: 300
-  },
-  tooltips: {
-    callbacks: {
-      label: (tooltipItem) => numberWithCommas(tooltipItem.yLabel.toString())
-    }
-  }
-}
+Chart.defaults.global.animation = false
+Chart.defaults.global.tooltips.callbacks.label = (tooltipItem) => numberWithCommas(tooltipItem.yLabel.toString())
 
 const straightLineGraphOption = {
   elements: {
@@ -50,41 +40,98 @@ const createValueBadge = (value, suffix) => value >= 0
 
 const safeDate = (date) => date || new Date()
 
-const XpShow = ({ children, start, end, name, skill, ranks, skillRank, skillXp, allRanks, allXp }) => (
-  <div style={{height: 'inherit'}}>
-    <Layout fullWidth>
-      <Helmet>
-        <title>Experience Tracker - {hero.title}</title>
-      </Helmet>
-      <h1>{name} / <small className='text-muted'>
-        {skill} / {safeDate(start).toDateString().toLowerCase()} / {safeDate(end).toDateString().toLowerCase()}</small>
-      </h1>
-      <hr />
-      <div className='row'>
-        <div className='col-md-3 col-sm-4 col-xs-12'>
-          <ul className='list-group'>
-            {ranks.map(({skill: skillName, rank, xp}) => (
-              <a
-                className={'list-group-item list-group-item-action ' + (skill === skillName ? 'active' : '')}
-                key={skillName}
-                href={`/xp/show/${skillName}/${name}/${safeDate(start).getTime()}/${safeDate(end).getTime()}`}>
-                <img alt={skillName} src={`/img/skillicons/${skillName}.png`} /> {capitalizeFirstLetter(skillName)} <br />
-                {createValueBadge(rank, 'ranks')} {createValueBadge(xp, 'xp')}
-              </a>
-            ))}
-          </ul>
-        </div>
-        <div className='col-md-9 col-sm-8 col-xs-12'>
-          <Line data={skillRank} options={reverseGraphOptions} />
-          <Line data={skillXp} options={straightLineGraphOption} />
-          <Bar data={allXp} />
-          <Bar data={allRanks} />
-        </div>
+class XpShow extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      skillRank: null,
+      skillXp: null,
+      allRanks: null,
+      allXp: null
+    }
+  }
+
+  componentWillReceiveProps () {
+    this.setState({
+      skillRank: new Chart('skill-rank', {
+        type: 'line',
+        data: this.props.skillRank,
+        options: reverseGraphOptions
+      }),
+      skillXp: new Chart('skill-xp', {
+        type: 'line',
+        data: this.props.skillXp,
+        options: straightLineGraphOption
+      }),
+      allRanks: new Chart('all-ranks', {
+        type: 'bar',
+        data: this.props.allRanks
+      }),
+      allXp: new Chart('all-xp', {
+        type: 'bar',
+        data: this.props.allXp
+      })
+    })
+  }
+
+  componentWillUnmount () {
+    if (this.state.skillRank) {
+      this.state.skillRank.destroy()
+    }
+
+    if (this.state.skillXp) {
+      this.state.skillXp.destroy()
+    }
+
+    if (this.state.allRanks) {
+      this.state.allRanks.destroy()
+    }
+
+    if (this.state.allXp) {
+      this.state.allXp.destroy()
+    }
+  }
+
+  render ({children, start, end, name, skill, ranks}) {
+    return (
+      <div style={{height: 'inherit'}}>
+        <Layout fullWidth>
+          <Helmet>
+            <title>Experience Tracker - {hero.title}</title>
+          </Helmet>
+          <h1>{name} / <small className='text-muted'>
+            {skill} / {safeDate(start).toDateString().toLowerCase()} / {safeDate(end).toDateString().toLowerCase()}</small>
+          </h1>
+          <hr />
+          <div className='row'>
+            <div className='col-md-3 col-sm-4 col-xs-12'>
+              <ul className='list-group'>
+                {ranks.map(({skill: skillName, rank, xp}) => (
+                  <NavLink
+                    className={'list-group-item list-group-item-action ' + (skill === skillName ? 'active' : '')}
+                    key={skillName}
+                    to={`/xp/show/${skillName}/${name}/${safeDate(start).getTime()}/${safeDate(end).getTime()}`}>
+                    <img alt={skillName} src={`/img/skillicons/${skillName}.png`} /> {capitalizeFirstLetter(skillName)}
+                    <br />
+                    {createValueBadge(rank, 'ranks')} {createValueBadge(xp, 'xp')}
+                  </NavLink>
+                ))}
+              </ul>
+            </div>
+            <div className='col-md-9 col-sm-8 col-xs-12'>
+              <canvas id='skill-rank' />
+              <canvas id='skill-xp' />
+              <canvas id='all-xp' />
+              <canvas id='all-ranks' />
+            </div>
+          </div>
+          {children}
+        </Layout>
       </div>
-      {children}
-    </Layout>
-  </div>
-)
+    )
+  }
+}
 
 export default connect(
   (state, props) => ({
