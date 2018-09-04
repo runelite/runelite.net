342c16a79408f8a59a8efd034ec3709cfd4e25eb
Tomas Slusny <slusnucky@gmail.com>
2018-09-04 13:42:06 +0200

    Add sitemap.xml
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- config-overrides.js
+++ config-overrides.js
@@ -1,23 +1,33 @@
 const fs = require('fs')
 const path = require('path')
 const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const SitemapPlugin = require('sitemap-webpack-plugin').default
 const rewirePreact = require('react-app-rewire-preact')
 const rewireEslint = require('react-app-rewire-eslint')
+const hero = require('./src/_data/hero')
 
 module.exports = function override (config, env) {
   config = rewirePreact(config, env)
   config = rewireEslint(config, env)
 
   const posts = fs.readdirSync(path.join('src', '_posts'))
+  const routes = ['/', '/features', '/blog'].concat(
+    posts.map(p => '/blog/show/' + p.replace('.md', ''))
+  )
 
   config.plugins.push(
     new PrerenderSPAPlugin({
       // Required - The path to the webpack-outputted app to prerender.
       staticDir: path.join(__dirname, 'build'),
       // Required - Routes to render.
-      routes: ['/', '/features', '/blog'].concat(
-        posts.map(p => '/blog/show/' + p.replace('.md', ''))
-      )
+      routes
+    })
+  )
+
+  config.plugins.push(
+    new SitemapPlugin(hero.url, routes.map(path => ({ path })), {
+      lastMod: true,
+      changeFreq: 'weekly'
     })
   )
 
