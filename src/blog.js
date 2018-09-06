06f7adde4f3f1cd8f339e271a710b0e8c6ddd8d7
Tomas Slusny <slusnucky@gmail.com>
2018-09-06 14:59:43 +0200

    Remove time from blog links
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/blog.js
+++ src/blog.js
@@ -5,26 +5,30 @@ const blog = require.context('!null-loader!./_posts', false, /.md$/).keys()
     // Remove cd and extension
     fileName = fileName.match(/([\w\d-.]+)\.md/)[1]
 
+    // Extract date and path
+    const tokenizedFilename = fileName.match(
+      /^(\d{4}-\d{2}-\d{2})-(\d{2}-\d{2})(.*)/
+    )
+
+    // Validation
+    if (
+      !tokenizedFilename ||
+      !tokenizedFilename[1] ||
+      !tokenizedFilename[2] ||
+      !tokenizedFilename[3]
+    ) {
+      throw new Error('no ^YYYY-MM-DD-HH-mm date in blog filename')
+    }
+
+    const date = tokenizedFilename[1]
+    const time = tokenizedFilename[2]
+    const name = tokenizedFilename[3]
+
+    // This makes it so time is removed from path
+    const pathString = date + name
+
     const resolver = () => {
       const mapper = md => {
-        // Extract date and path
-        const tokenizedFilename = fileName.match(
-          /^(\d{4}-\d{2}-\d{2})-(\d{2}-\d{2})(.*)/
-        )
-
-        // Validation
-        if (
-          !tokenizedFilename ||
-          !tokenizedFilename[1] ||
-          !tokenizedFilename[2] ||
-          !tokenizedFilename[3]
-        ) {
-          throw new Error('no ^YYYY-MM-DD-HH-mm date in blog filename')
-        }
-
-        const date = tokenizedFilename[1]
-        const time = tokenizedFilename[2]
-
         // Frontmatter and content (actual markdown is loaded on '__content', frontmatter is right on root)
         const dateTime = date + '-' + time
         const dateArray = dateTime.split('-')
@@ -55,7 +59,7 @@ const blog = require.context('!null-loader!./_posts', false, /.md$/).keys()
       return import(`!markdown-with-front-matter-loader!./_posts/${fileName}.md`).then(mapper)
     }
 
-    return memo.set(fileName, resolver)
+    return memo.set(pathString, resolver)
   }, new Map())
 
 export const latest = blog.values().next().value
