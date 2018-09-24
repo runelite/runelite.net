7029c0662a17224bd4b3868fb0f44eac8d5bb54d
Tomas Slusny <slusnucky@gmail.com>
2018-09-24 17:36:10 +0200

    Add support for styling <Layout>
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,19 @@
 import { h } from 'preact'
 import Footer from './footer'
 
-const Layout = ({ children, fullWidth }) => (
-  <div
-    class="container"
-    id="layout"
-    style={{ maxWidth: fullWidth ? '100%' : '' }}
-  >
-    {children}
-    <Footer />
-  </div>
-)
+const Layout = ({ children, fullWidth, ...props }) => {
+  props.class = props.class ? 'container ' + props.class : 'container'
+  props.style = {
+    ...(props.style || {}),
+    maxWidth: fullWidth ? '100%' : ''
+  }
+
+  return (
+    <div {...props} id="layout">
+      {children}
+      <Footer />
+    </div>
+  )
+}
 
 export default Layout
