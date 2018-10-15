7920be24cc0b87326d95575d74b5dfb497718f2a
Tomas Slusny <slusnucky@gmail.com>
2018-10-15 16:01:12 +0200

    Add /tab/show/:csv for showing tag tab
    Example:
    ```
    http://localhost:3000/tag/show/raids,19481,8842,3024,22109,12899,11771,19553,19547,12018,12006,12002,13441,12695,6739,5952,21795,12625,12612,11889,6570,12954,12926,11804,12797,12791,19933,13073,13072,19484,19481,11665,11664,11663,13393,2444,6685
    ```
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

---
+++ src/routes/tag-show.js
@@ -0,0 +1,79 @@
+import { Component, h } from 'preact'
+import Layout from '../components/layout'
+import hero from '../_data/hero'
+import Meta from '../components/meta'
+import { bindActionCreators } from 'redux'
+import { getReleases, latestReleaseSelector } from '../modules/git'
+import { getItemInfo } from '../modules/runelite'
+import { connect } from 'preact-redux'
+import './tag-show.css'
+
+class TagShow extends Component {
+  componentDidMount() {
+    this.props
+      .getReleases()
+      .then(() => this.props.getItemInfo(this.props.itemIds))
+  }
+
+  render({ name, icon, itemIds, items, version, csv }) {
+    return (
+      <div>
+        <Meta title={`${name} tag tab - ${hero.title}`} />
+        <Layout>
+          <h1>
+            <img
+              src={`https://api.runelite.net/runelite-${
+                version.name
+              }/cache/item/${icon}/image`}
+            />{' '}
+            {name}
+          </h1>
+          <hr />
+          <div class="row">
+            <pre>{csv}</pre>
+            {itemIds.map(id => {
+              const item = items.find(i => i.id == id) || {}
+              const name = item.name || ''
+              const nameSan = name.replace(' ', '_')
+
+              return (
+                <div class="card">
+                  <a
+                    href={`https://oldschool.runescape.wiki/w/${nameSan}`}
+                    title={name}
+                  >
+                    <img
+                      class="card-img-top"
+                      alt={name}
+                      title={name}
+                      src={`https://api.runelite.net/runelite-${
+                        version.name
+                      }/cache/item/${id}/image`}
+                    />
+                  </a>
+                </div>
+              )
+            })}
+          </div>
+        </Layout>
+      </div>
+    )
+  }
+}
+
+export default connect(
+  (state, { csv }) => {
+    const parts = csv.split(',')
+    const name = parts.shift()
+    const icon = parts.shift()
+
+    return {
+      name,
+      icon,
+      itemIds: parts,
+      items: state.runelite.items || [],
+      version: latestReleaseSelector(state)
+    }
+  },
+  dispatch => bindActionCreators({ getReleases, getItemInfo }, dispatch)
+)(TagShow)
