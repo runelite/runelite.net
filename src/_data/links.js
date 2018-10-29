ea3fbd75033431924dfc2cfd14d758aec47fd01b
psikoi <ruben.amendoeira@gmail.com>
2018-10-29 11:39:34 +0000

    Update after rebase

--- src/_data/links.js
+++ src/_data/links.js
@@ -2,6 +2,5 @@ export default {
   github: 'https://github.com/runelite',
   discord: 'https://discord.gg/mePCs8U',
   patreon: 'https://www.patreon.com/runelite',
-  twitter: 'https://twitter.com/RuneLiteClient',
-  github: 'https://github.com/runelite'
+  twitter: 'https://twitter.com/RuneLiteClient'
 }
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,18 +1,19 @@
 import { h } from 'preact'
 import './navigation.css'
 import hero from '../_data/hero'
+import links from '../_data/links.js'
 import { Link } from 'preact-router'
 
 const Navigation = ({ stars, dark }) => (
   <nav
     class={
       'navbar navbar-expand-lg fixed-top ' +
-      (dark ? 'navbar-gradient navbar-dark' : 'navbar-light bg-white')
+      (dark ? 'navbar-transparent' : 'navbar-dark')
     }
   >
     <Link class="navbar-brand" activeClassName="active" href="/">
-      <img src={hero.logo} class="icon" alt="RuneLite" /> Home
-      </Link>
+      <img src={hero.logo} class="icon" alt="Logo" />
+    </Link>
     <input type="checkbox" id="navbar-check-box" />
     <label for="navbar-check-box" class="navbar-toggler">
       <span class="navbar-toggler-icon" />
@@ -35,7 +36,6 @@ const Navigation = ({ stars, dark }) => (
           </Link>
         </li>
         <li class="nav-item dropdown">
-          {/* eslint-disable-next-line */}
           <a class="nav-link dropdown-toggle" id="navbarDropdown">
             API
           </a>
@@ -55,42 +55,12 @@ const Navigation = ({ stars, dark }) => (
           </div>
         </li>
         <li class="nav-item">
-          <Link class="nav-link" activeClassName="active" href="/tag">
-            <i class="fas fa-code" /> Tags
-          </Link>
-        </li>
-      </ul>
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <a class="nav-link" href={links.discord} title="Discord">
-            <i class="fab fa-discord" />
-            <span class="d-lg-none"> Discord</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href={links.twitter} title="Twitter">
-            <i class="fab fa-twitter" />
-            <span class="d-lg-none"> Twitter</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href={links.github} title="GitHub">
-            <i class="fab fa-github" />
-            <span class="d-lg-none"> GitHub</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href={links.patreon}>
-            <i class="fab fa-patreon" /> Become a patron
-          </a>
-        </li>
-        <li class="nav-item">
           <a class="nav-link" href="https://github.com/runelite/runelite/wiki">
             Wiki
           </a>
         </li>
         <li class="nav-item">
-          <a href="/" role="button" class="btn btn-primary btn-sm">
+          <a href="/" role="button" class="btn btn-primary btn-sm cta-button">
             Download
           </a>
         </li>
