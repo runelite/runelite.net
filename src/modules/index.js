7b5f042f34ba0064338c5fb5534f65083c5e9a27
Tomas Slusny <slusnucky@gmail.com>
2019-02-09 00:40:50 +0100

    Split Item module from RuneLite module
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,5 +1,6 @@
 import appReducer from './app'
 import gitReducer from './git'
+import itemReducer from './item'
 import runeliteReducer from './runelite'
 import tagReducer from './tag'
 import xpReducer from './xp'
@@ -8,6 +9,7 @@ import xpReducer from './xp'
 export default {
   app: appReducer,
   git: gitReducer,
+  item: itemReducer,
   runelite: runeliteReducer,
   tag: tagReducer,
   xp: xpReducer
--- src/modules/runelite.js
+++ src/modules/runelite.js
@@ -1,18 +1,11 @@
-import { uniq, concat } from 'ramda'
 import { createActions, handleActions } from 'redux-actions'
 import api from '../api'
 import { startLoading, stopLoading } from './app'
 
 const runeliteApi = api('https://api.runelite.net/')
-const runeliteStaticApi = api('https://static.runelite.net/')
 
 // Actions
-export const {
-  getSessionCount,
-  getItemInfo,
-  setSessionCount,
-  setItemInfo
-} = createActions(
+export const { getSessionCount, setSessionCount } = createActions(
   {
     GET_SESSION_COUNT: () => async dispatch => {
       dispatch(startLoading())
@@ -24,36 +17,9 @@ export const {
       dispatch(setSessionCount(response))
       dispatch(stopLoading())
       return response
-    },
-    GET_ITEM_INFO: items => async dispatch => {
-      dispatch(startLoading())
-      const version = latestReleaseSelector(getState()).name
-      const names = await runeliteStaticApi('cache/item/names.json', {
-        method: 'GET'
-      })
-
-      const result = await Promise.all(
-        items.map(item =>
-          runeliteApi(`runelite-${version}/examine/item/${item}`, {
-            method: 'GET'
-          }).then(examine => {
-            dispatch(
-              setItemInfo({
-                id: item,
-                name: names[item],
-                examine
-              })
-            )
-          })
-        )
-      )
-
-      dispatch(stopLoading())
-      return result
     }
   },
-  'SET_SESSION_COUNT',
-  'SET_ITEM_INFO'
+  'SET_SESSION_COUNT'
 )
 
 // Reducer
@@ -62,15 +28,10 @@ export default handleActions(
     [setSessionCount]: (state, { payload }) => ({
       ...state,
       sessionCount: payload
-    }),
-    [setItemInfo]: (state, { payload }) => ({
-      ...state,
-      items: uniq(concat(state.items, [payload]))
     })
   },
   {
-    sessionCount: 0,
-    items: []
+    sessionCount: 0
   }
 )
 
