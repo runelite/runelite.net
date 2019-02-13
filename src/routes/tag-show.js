38e18b858f409b737fb4f21025e31d474f658e0f
Tomas Slusny <slusnucky@gmail.com>
2019-02-13 09:00:04 +0100

    Migrate TagShow route to prepare HOC
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/routes/tag-show.js
+++ src/routes/tag-show.js
@@ -1,4 +1,4 @@
-import { Component, h } from 'preact'
+import { h } from 'preact'
 import Layout from '../components/layout'
 import hero from '../_data/hero'
 import Meta from '../components/meta'
@@ -8,74 +8,75 @@ import { fetchItemInfo } from '../modules/item'
 import { connect } from 'preact-redux'
 import '../components/tooltip.css'
 import './tag.css'
+import prepare from '../components/prepare'
 
 const formatIcon = icon =>
   `https://static.runelite.net/cache/item/icon/${icon}.png`
 
-class TagShow extends Component {
-  componentDidMount() {
-    this.props
-      .fetchReleases()
-      .then(() => this.props.fetchItemInfo(this.props.itemIds))
-  }
+const TagShow = ({ name, icon, itemIds, items, version, csv }) => (
+  <div>
+    <Meta title={`${name} tag tab - ${hero.title}`} />
+    <Layout class="tag-container">
+      <h1>
+        <img alt="" src={formatIcon(icon)} /> {name}
+      </h1>
+      <hr />
+      <div class="row">
+        <pre class="pre-select">{csv}</pre>
+        {itemIds.map(id => {
+          const item = items.find(i => i.id === id) || {}
+          const name = item.name || ''
+          const examine = item.examine || ''
+          const nameSan = name.replace(' ', '_')
 
-  render({ name, icon, itemIds, items, version, csv }) {
-    return (
-      <div>
-        <Meta title={`${name} tag tab - ${hero.title}`} />
-        <Layout class="tag-container">
-          <h1>
-            <img alt="" src={formatIcon(icon)} /> {name}
-          </h1>
-          <hr />
-          <div class="row">
-            <pre class="pre-select">{csv}</pre>
-            {itemIds.map(id => {
-              const item = items.find(i => i.id === id) || {}
-              const name = item.name || ''
-              const examine = item.examine || ''
-              const nameSan = name.replace(' ', '_')
-
-              return (
-                <div class="card">
-                  <div class="tooltip-tag">
-                    <a href={`https://oldschool.runescape.wiki/w/${nameSan}`}>
-                      <img
-                        class="card-img-top"
-                        alt={name}
-                        src={formatIcon(id)}
-                      />
-                    </a>
-                    <div class="tooltip-tag-text">
-                      <b>{item.name || 'Loading...'}</b>
-                      <br />
-                      <small>{examine}</small>
-                    </div>
-                  </div>
+          return (
+            <div class="card">
+              <div class="tooltip-tag">
+                <a href={`https://oldschool.runescape.wiki/w/${nameSan}`}>
+                  <img class="card-img-top" alt={name} src={formatIcon(id)} />
+                </a>
+                <div class="tooltip-tag-text">
+                  <b>{item.name || 'Loading...'}</b>
+                  <br />
+                  <small>{examine}</small>
                 </div>
-              )
-            })}
-          </div>
-        </Layout>
+              </div>
+            </div>
+          )
+        })}
       </div>
-    )
+    </Layout>
+  </div>
+)
+
+const mapStateToProps = (state, { csv }) => {
+  let parts = csv.split(',')
+  const name = parts.shift()
+  const icon = parts.shift()
+  parts = parts.map(id => Math.abs(parseInt(id, 10))).sort((a, b) => a - b)
+
+  return {
+    name,
+    icon,
+    itemIds: parts,
+    items: state.item || [],
+    version: getLatestRelease(state)
   }
 }
 
-export default connect(
-  (state, { csv }) => {
-    let parts = csv.split(',')
-    const name = parts.shift()
-    const icon = parts.shift()
-    parts = parts.map(id => Math.abs(parseInt(id, 10))).sort((a, b) => a - b)
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ fetchReleases, fetchItemInfo }, dispatch)
 
-    return {
-      name,
-      icon,
-      itemIds: parts,
-      items: state.item || [],
-      version: getLatestRelease(state)
-    }
-  },
-  dispatch => bindActionCreators({ fetchReleases, fetchItemInfo }, dispatch)
-)(TagShow)
+const prepareComponentData = async ({
+  fetchReleases,
+  fetchItemInfo,
+  itemIds
+}) => {
+  await fetchReleases()
+  await fetchItemInfo(itemIds)
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(prepare(prepareComponentData)(TagShow))
