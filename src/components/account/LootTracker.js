da1544b3e5ee77215932ab7daea829844715ce03
Max Weber <mii7303@gmail.com>
2019-02-22 20:35:19 -0700

    Use spritesheet item qtys

--- src/components/account/LootTracker.js
+++ src/components/account/LootTracker.js
@@ -4,12 +4,71 @@ import './loot-tracker.css'
 
 const getRlIcon = id => `https://static.runelite.net/cache/item/icon/${id}.png`
 
+const glyphMap = {
+  '1': { x: 0, y: 0, w: 5 },
+  '2': { x: 5, y: 0, w: 7 },
+  '3': { x: 12, y: 0, w: 6 },
+  '4': { x: 18, y: 0, w: 6 },
+  '5': { x: 24, y: 0, w: 6 },
+  '6': { x: 30, y: 0, w: 7 },
+  '7': { x: 37, y: 0, w: 6 },
+  '8': { x: 43, y: 0, w: 7 },
+  '9': { x: 50, y: 0, w: 7 },
+  '0': { x: 57, y: 0, w: 7 },
+  M: { x: 0, y: 10, w: 8 },
+  K: { x: 8, y: 10, w: 7 },
+  '%': { x: 15, y: 10, w: 7 },
+  '-': { x: 22, y: 10, w: 5 },
+  '+': { x: 27, y: 10, w: 7 },
+  '/': { x: 37, y: 10, w: 5 },
+  '*': { x: 39, y: 10, w: 7 },
+  '=': { x: 46, y: 0, w: 6 },
+  '^': { x: 52, y: 10, w: 5 },
+  '(': { x: 57, y: 10, w: 4 },
+  ')': { x: 60, y: 10, w: 4 }
+}
+
+const quantityNums = ['', 'K', 'M']
+
+const buildQuantity = num => {
+  let l = 0
+  while (num > 9999) {
+    num /= 1000
+    l++
+  }
+
+  const si = quantityNums[l]
+  const str = ~~num + si
+
+  return (
+    <span class={`rs-item-quantity rs-item-quantity-${si || 'none'}`}>
+      {[...str]
+        .map(g => glyphMap[g])
+        .filter(g => !!g)
+        .map(g => (
+          <span
+            class="rs-item-quantity-number"
+            style={{
+              width: g.w,
+              backgroundPositionX: -g.x,
+              backgroundPositionY: -g.y
+            }}
+          />
+        ))}
+    </span>
+  )
+}
+
 const buildDrop = drop => (
   <div class="drop-wrapper">
-    <span class="drop-quantity">{drop.qty}</span>
+    {buildQuantity(drop.qty)}
     <div class="tooltip-tag">
       <a href={`https://oldschool.runescape.wiki/w/${drop.name}`}>
-        <img class="card-img-top" alt={drop.id} src={getRlIcon(drop.id)} />
+        <img
+          class="card-img-top rs-icon"
+          alt={drop.id}
+          src={getRlIcon(drop.id)}
+        />
       </a>
       <div class="tooltip-tag-text">
         <b>{drop.name || 'Loading...'}</b>
