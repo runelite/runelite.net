816130855e7b364f37d714d73d89ac5d0e51a4af
Tomas Slusny <slusnucky@gmail.com>
2019-02-23 02:39:39 +0100

    Do not use inline styles
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/components/account/LootTracker.js
+++ src/components/account/LootTracker.js
@@ -1,31 +1,15 @@
 import { Component, h } from 'preact'
 import '../tooltip.css'
+import './loot-tracker.css'
 
-const getRlIcon = id =>
-  `https://static.runelite.net/cache/item/icon/${parseInt(id, 10)}.png`
+const getRlIcon = id => `https://static.runelite.net/cache/item/icon/${id}.png`
 
 const buildDrop = drop => (
-  <div style={{ border: 'none', position: 'relative' }}>
-    <span
-      style={{
-        position: 'absolute',
-        left: 4,
-        top: -2,
-        color: 'yellow',
-        textShadow: 'black 1px 1px',
-        fontSize: 'small'
-      }}
-    >
-      {drop.qty}
-    </span>
+  <div class="drop-wrapper">
+    <span class="drop-quantity">{drop.qty}</span>
     <div class="tooltip-tag">
       <a href={`https://oldschool.runescape.wiki/w/${drop.name}`}>
-        <img
-          class="card-img-top"
-          style={{ padding: 2 }}
-          alt={drop.id}
-          src={getRlIcon(drop.id)}
-        />
+        <img class="card-img-top" alt={drop.id} src={getRlIcon(drop.id)} />
       </a>
       <div class="tooltip-tag-text">
         <b>{drop.name || 'Loading...'}</b>
@@ -40,13 +24,8 @@ const buildLootRecord = record => (
       {record.name}
       <span class="small float-right">x{record.count}</span>
     </div>
-    <div class="card-body pt-0 pb-0" style={{ border: 'none' }}>
-      <div
-        class="row"
-        style={{ marginLeft: -10, paddingTop: 5, paddingBottom: 5 }}
-      >
-        {record.drops.map(buildDrop)}
-      </div>
+    <div class="card-body pt-0 pb-0 record-body">
+      <div class="row drop-row">{record.drops.map(buildDrop)}</div>
     </div>
   </div>
 )
