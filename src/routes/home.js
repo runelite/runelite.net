bbd00794054329cd4fb830183c518a791a7e6dd2
psikoi <ruben.amendoeira@gmail.com>
2019-02-26 00:12:02 +0000

    Add about section

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,7 +1,9 @@
 import { h } from 'preact'
+import './home.css'
 import { connect } from 'preact-redux'
 import Feature from '../components/feature'
 import Layout from '../components/layout'
+import links from '../_data/links'
 import Hero from '../components/hero'
 import { latest } from '../blog'
 import {
@@ -33,6 +35,54 @@ const Home = ({ commit, release, sessionCount }) => (
       playing={sessionCount}
     />
     <Layout>
+      <Hero
+        {...hero}
+        release={release.name}
+        stars={stars}
+        commit={commit}
+        playing={sessionCount}
+      />
+
+      <section id="about">
+        <div class="content-section">
+          <div class="video">
+            <iframe
+              width="100%"
+              height="100%"
+              src="https://www.youtube.com/embed/l2vUWMyQ594"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            />
+          </div>
+
+          <div class="info">
+            <h1>WHAT IS RUNELITE</h1>
+            <p style="white-space: pre-line">
+              Curabitur pretium ut risus non eleifend. Vestibulum ante ipsum
+              primis in faucibus orci luctus et ultrices posuere cubilia Curae.
+              {'\n'}
+              {'\n'}
+              In hac habitasse platea dictumst. Duis diam metus, semper dictum
+              urna in, finibus feugiat ligula. Donec lacinia nec erat vel
+              tempor. Proin euismod nunc quis diam maximus malesuada.
+            </p>
+            <a href={links.github} title="GitHub">
+              <i class="fab fa-github" />
+            </a>
+            <a href={links.discord} title="Discord">
+              <i class="fab fa-discord" />
+            </a>
+            <a href={links.twitter} title="Twitter">
+              <i class="fab fa-twitter" />
+            </a>
+            <a href={links.patreon} title="Patreon">
+              <i class="fab fa-patreon" />
+            </a>
+          </div>
+        </div>
+      </section>
+
       <h1>
         Features{' '}
         <Link href="/features" style={{ fontSize: 18 }}>
