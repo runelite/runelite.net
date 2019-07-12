42b6f235029952f13b6fc10ffc0cfc3e7a7302d0
psikoi <ruben.amendoeira@gmail.com>
2019-07-12 22:59:20 +0100

    Restyle Account/GrandExchange

--- src/routes/account/loot-tracker.js
+++ src/routes/account/loot-tracker.js
@@ -12,6 +12,7 @@ import { fetchReleases } from '../../modules/git'
 import { bindActionCreators } from 'redux'
 import prepare from '../../components/prepare'
 import { wikiURLForItem } from '../../util'
+import SearchBar from '../../components/search-bar'
 
 const getRlIcon = id => `https://static.runelite.net/cache/item/icon/${id}.png`
 
@@ -107,21 +108,10 @@ const handleChange = (event, setLootFilter) =>
 
 const LootTracker = ({ loot, lootFilter, setLootFilter }) => (
   <div>
-    <div class="search input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text">
-          <i class="fas fa-search" />
-        </span>
-      </div>
-      <input
-        type="text"
-        class="form-control"
-        placeholder="Search..."
-        value={lootFilter.name}
-        onInput={e => handleChange(e, setLootFilter)}
-      />
-    </div>
-
+    <SearchBar
+      value={lootFilter.name}
+      onInput={e => handleChange(e, setLootFilter)}
+    />
     <div class="card-columns">{loot.map(buildLootRecord)}</div>
   </div>
 )
