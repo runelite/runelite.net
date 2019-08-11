e636c2877a89b3c067b895157c79b1737ff8579d
Tomas Slusny <slusnucky@gmail.com>
2019-08-12 01:54:49 +0200

    Add launcher redirects

---
+++ redirect.js
@@ -0,0 +1,8 @@
+module.exports = {
+  prefix: 'redirect',
+  links: {
+    'launcher/discord': 'https://discord.gg/ArdAhnN',
+    'launcher/troubleshooting':
+      'https://github.com/runelite/runelite/wiki/Troubleshooting-problems-with-the-client'
+  }
+}
