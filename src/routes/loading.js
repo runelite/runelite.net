8f9a6e8003d79aa416805d24fcb580187d9d19bc
gc <gc@users.noreply.github.com>
2019-10-05 02:07:28 +1000

    fix: page still has old design css

--- src/routes/loading.js
+++ src/routes/loading.js
@@ -2,14 +2,19 @@ import { h } from 'preact'
 import Layout from '../components/layout'
 import hero from '../_data/hero'
 import Meta from '../components/meta'
+import './loading.scss'
 
 const Loading = () => (
   <div>
     <Meta title={`Loading - ${hero.title}`} />
     <Layout>
-      <h1>Loading...</h1>
-      <hr />
-      <div>Please wait while you are redirected.</div>
+      <section id="loading">
+        <div class="content-section">
+          <h1>Loading...</h1>
+          <hr />
+          <p>Please wait while you are redirected.</p>
+        </div>
+      </section>
     </Layout>
   </div>
 )
