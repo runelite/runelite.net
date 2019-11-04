94d98a4d69d589b11e4fe44ad6e618939b3b1e7b
Tomas Slusny <slusnucky@gmail.com>
2019-11-05 00:48:57 +0100

    Use /discord link for Discord invites in navbar
    - Switch from links.discord to /discord redirect in navbar
    - Remove links.discord
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/_data/links.js
+++ src/_data/links.js
@@ -1,5 +1,4 @@
 export default {
-  discord: 'https://discord.gg/mePCs8U',
   patreon: 'https://www.patreon.com/runelite',
   twitter: 'https://twitter.com/RuneLiteClient',
   github: 'https://github.com/runelite'
