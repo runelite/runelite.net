56cd543aa30bf0ee30fd7b2e682e7a577f17ed71
Hydrox6 <ikada@protonmail.ch>
2021-04-29 12:38:30 +0100

    update discord links to use a link that starts users in #welcome

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 module.exports = {
-  discord: 'https://discord.gg/mePCs8U',
+  discord: 'https://discord.gg/ArdAhnN',
   'redirect/launcher/discord': 'https://discord.gg/ArdAhnN',
   'redirect/launcher/troubleshooting':
     'https://github.com/runelite/runelite/wiki/Troubleshooting-problems-with-the-client'
