0d7559fde613a24b3809cee0714dfbffefa51ead
Tomas Slusny <slusnucky@gmail.com>
2021-06-30 04:27:53 +0200

    Properly handle scrolling
    Reset scroll on navigation, remember scroll on browser navigation back
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,6 +6,24 @@ import configureStore from './store'
 import { sessionCheck } from './modules/account'
 import { fetchBootstrap } from './modules/bootstrap'
 
+// Properly handle scrolling
+if (window && window.history) {
+  const scrollHistory = {}
+
+  let { pushState } = window.history
+  window.history.pushState = (a, b, url) => {
+    scrollHistory[window.location.pathname] =
+      window.scrollY || window.pageYOffset
+    pushState.call(window.history, a, b, url)
+    if (url.indexOf('#') < 0) window.scrollTo(0, 0)
+  }
+
+  window.onpopstate = () => {
+    const y = scrollHistory[window.location.pathname] || 0
+    window.setTimeout(() => window.scrollTo(0, y), 1)
+  }
+}
+
 // Check session and get API version
 const callback = async store => {
   await store.dispatch(fetchBootstrap())
