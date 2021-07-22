ed49f4349fc5037a88306dac83e7e7f323de8b21
Tomas Slusny <slusnucky@gmail.com>
2021-07-22 16:39:29 +0200

    Fix hash navigation
    Closes #431
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/index.js
+++ src/index.js
@@ -15,10 +15,14 @@ if (window && window.history) {
     scrollHistory[window.location.pathname] =
       window.scrollY || window.pageYOffset
     pushState.call(window.history, a, b, url)
-    if (url.indexOf('#') < 0) window.scrollTo(0, 0)
+    if (!url.includes('#')) window.scrollTo(0, 0)
   }
 
   window.onpopstate = () => {
+    if (window.location.hash) {
+      return
+    }
+
     const y = scrollHistory[window.location.pathname] || 0
     window.setTimeout(() => window.scrollTo(0, y), 1)
   }
