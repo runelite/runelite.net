9462e2a26c29720224a13fa722e53012ec535163
Tomas Slusny <slusnucky@gmail.com>
2021-09-21 01:24:23 +0200

    Move sha256sums to separate file
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/routes/verify.js
+++ src/routes/verify.js
@@ -1,6 +1,7 @@
 import './verify.css'
 import Layout from '../components/layout'
 import hero from '../_data/hero'
+import sha256sums from '../_data/sha256sums'
 import { h, Component, Fragment } from 'preact'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
@@ -16,25 +17,6 @@ const FILE_STATE = Object.freeze({
   INVALID: Symbol('INVALID'),
   VALID: Symbol('VALID')
 })
-
-// Hardcoded sha256sums of launcher executables and pre-installer launchers
-const LAUNCHER_HASHES = [
-  '020004743a7e0bf6d257ef0833b9ed229204a0112e80a05adfed36d73d7c3d5e',
-  '07c658c06b48637409a27fa299dbb447ea2a047db46fa726ea208fda5b8e4cef',
-  '2cea36b447a507c793367111a6d891de6fa028440daba22aa24a504034816fbd',
-  '36da47090fa642a4ae9bdc28f7ba00fda16dbf363064c96a5132a3f00fe4685d',
-  '3e4d8ba370c6369962279d88de7371f9a285a5c1eeced2f3f0507acd8265850a',
-  '49ab6cb2e916b1dd62db1f52f1223bc47226a226abd7f1e3d0c9a34351eb23d0',
-  '7b6a27fcae09b79db920a402be8d8f69249257b2cc491fb1372b2852aaf67341',
-  '8715ddf633d4e4fe9f928cec84b20df1eedb630ef6e3b5d826039f3cbd9268a2',
-  '8865dd1b9c902e89a1906159ed097242a49786652dc5a0407d8316e4ef56369b',
-  '8edf21633c91a2a360cb3433f9322d3e3cd1fc2f198dc95f64d2e7ea8ff147e9',
-  '9041dcc4ebeb326e02744e10391e90fe96d7fb56c9824fa283ec55321cf22455',
-  '96bd4957980d4cebee1c5978ce301e5950a50a6dfb40f191d91bc61a6777cd20',
-  'a202dea75936f16e11507ec6a666fc86c8d4ac9dec971be5930c470cd090bf23',
-  'b590edee17e2a4236b5a179d35f5934ddf9c9075fb09e93a7408f63242394a5f',
-  'bf7d1894f90d849619aab940c8d639340c2e745c1411e55d7735ac31398bc384'
-]
 
 const noop = e => {
   e.preventDefault()
@@ -68,7 +50,7 @@ class Verify extends Component {
       fileState = FILE_STATE.NONE
     } else if (name.match(/.*runelite.*\.lnk/i)) {
       fileState = FILE_STATE.SHORTCUT
-    } else if (LAUNCHER_HASHES.includes(sha256)) {
+    } else if (sha256sums.includes(sha256)) {
       fileState = FILE_STATE.LAUNCHER
     } else if (!!this.props.hashes[sha256]) {
       fileState = FILE_STATE.VALID
