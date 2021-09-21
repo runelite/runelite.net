47d004e8df7de979fe421fdd2977df5dd20f55dc
deathbeam <slusnucky@gmail.com>
2021-09-21 00:11:28 +0000

    Deploying to gh-pages from  @ 16741691fadda58320ab5971601b38ce2309e953 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1341c46aad9d0fdbdb4a00d08a48db9a.js"
+  "/precache-manifest.94a732e3bd68c01f6f6949a7aa64da21.js"
 );
 
 self.addEventListener('message', (event) => {
