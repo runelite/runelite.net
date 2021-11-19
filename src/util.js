799a5d4e678401cbcf0c7e749cb3c75c811c6bb1
Justin Nierendorf <justin.nierendorf@gmail.com>
2021-11-19 11:37:16 -0500

    Revert "use toLocaleString to format decimal places instead of hacky math"
    This reverts commit 790adfab66b06186883695d94776bd5169a40eb1.

--- src/util.js
+++ src/util.js
@@ -41,7 +41,8 @@ export const flattenMap = map => {
 // Convert number to MM:SS representation
 export const toMMSS = s => {
   const minutes = Math.floor(s / 60)
-  const seconds = (s % 60).toLocaleString(undefined, { maximumFractionDigits: 1, minimumFractionDigits: 0 });
+  // Weird arithmetic is needed here to prevent rounding errors due to JS's floating point math
+  const seconds = Math.round(s % 60 * 10) / 10
   const minutesStr = String(minutes).padStart(2, '0')
   const secondsStr = String(seconds).padStart(2, '0')
   return minutesStr + ':' + secondsStr
