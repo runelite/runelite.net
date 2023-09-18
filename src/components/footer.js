4497cda46b5a3853cb89cce64373270ec3bbc7dc
Oskar Risberg <oskar.risberg@stendahls.se>
2023-09-18 10:26:56 +0200

    fix RSS link

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,7 +11,7 @@ const Footer = () => (
         <a href="https://getbootstrap.com/">Bootstrap</a>,{' '}
         <a href="https://reactjs.org/">React</a> and{' '}
         <a href="https://fontawesome.com/">Font Awesome</a>
-        <a href={`${getBaseUrl()}/atom.xml`} class="float-right">
+        <a href={`/atom.xml`} class="float-right">
           <i class="fas fa-rss" /> Subscribe via RSS
         </a>
       </footer>
